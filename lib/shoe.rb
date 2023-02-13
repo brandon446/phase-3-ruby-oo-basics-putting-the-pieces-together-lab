99bdf3b9e48be99a1977d38b63e9b0f1dd286992
brandon <brandon.ngongolo@student.moringaschool.com>
2023-02-13 07:54:56 -0500

    Complete

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand, :cobble
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand, condition = "old")
+        @brand = brand
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+   
+end
